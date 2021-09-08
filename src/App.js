8696312851bd10bb311e18de7c70b2d269f9f8d7
Ezhilarasi <tezzhilarasi@gmail.com>
2021-09-08 16:08:34 +0530

    hreuku url applied

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   };
   let Handlechange = async(e,id) => {
     try {
-     let updatedata= await axios.put(`http://localhost:3000/updatetask/${id}`,{status:e.target.checked})
+     let updatedata= await axios.put(`https://dolistapplication.herokuapp.com/${id}`,{status:e.target.checked})
      fetchdata();
 
     } catch (error) {
@@ -39,7 +39,7 @@ function App() {
   };
 let handledelete=async (id)=>{
 try {
-  let postdeletedata=await axios.delete(`http://localhost:3000/deletetask/${id}`)
+  let postdeletedata=await axios.delete(`https://dolistapplication.herokuapp.com/${id}`)
 fetchdata();
 } catch (error) {
   alert(error)
